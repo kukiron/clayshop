ba7373fdc44e4c53c3319a86074fa1148ebccaab
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-02-11 09:52:02 +0600

    Create store with middleware

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ import Navbar from "./components/Navbar"
 import rootReducer from "./data/reducers"
 import { AUTH_USER } from "./data/actions/constants"
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
-const store = createStoreWithMiddleware(rootReducer)
-const token = localStorage.getItem("token")
+const token = localStorage.getItem("token"),
+  store = createStore(rootReducer, {}, applyMiddleware(reduxThunk))
 
 // If we have a token, the user should be authenticated and app state updated
 token && store.dispatch({ type: AUTH_USER })
