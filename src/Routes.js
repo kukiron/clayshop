d9efbb9ab994d9a32b21ee1a8ab08710a9b64397
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-02-13 22:11:42 +0600

    Add DeleteUser route

--- src/Routes.js
+++ src/Routes.js
@@ -7,8 +7,14 @@ import ClayShop from "./pages/ClayShop"
 import Login from "./pages/auth/Login"
 import Logout from "./pages/auth/Logout"
 import Signup from "./pages/auth/Signup"
+import DeleteUser from "./pages/DeleteUser"
 import NotFound from "./pages/NotFound"
-import NewUserCreated, { AuthSuccess, AuthFailure } from "./pages/Redirects"
+import {
+  AuthSuccess,
+  AuthFailure,
+  NewUserCreated,
+  UserDeleted
+} from "./pages/Redirects"
 // HOCs
 import RequireAuth from "./wrappers/RequireAuth"
 import RequireAdmin from "./wrappers/RequireAdmin"
@@ -20,10 +26,12 @@ const Routes = () => (
     <Route path="/login" component={Login} />
     <Route path="/logout" component={Logout} />
     <Route path="/signup" component={RequireAdmin(Signup)} />
+    <Route path="/delete-user" component={RequireAdmin(DeleteUser)} />
     <Route path="/clayshop" component={RequireAuth(ClayShop)} />
     <Route path="/authorized" component={RequireAuth(AuthSuccess)} />
     <Route path="/unauthorized" component={RequireAuth(AuthFailure)} />
     <Route path="/user-created" component={RequireAdmin(NewUserCreated)} />
+    <Route path="/user-deleted" component={RequireAdmin(UserDeleted)} />
     <Route component={NotFound} />
   </Switch>
 )
