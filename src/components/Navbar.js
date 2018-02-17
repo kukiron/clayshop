a17c4a5645ec89aaad53d17dd1378a58865879e7
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-02-17 22:42:46 +0600

    Pass destructured props as arguments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,55 @@
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
 import { NavLink } from "react-router-dom"
 
-class Navbar extends Component {
+const NavBar = ({ authenticated, userAccess }) => {
   // Navbar for authenticated users
-  authenticatedUser() {
-    return [
-      <li key={2}>
-        <NavLink to="/clayshop">ClayShop</NavLink>
-      </li>,
-      <li key={1}>
-        <NavLink to="/logout">Logout</NavLink>
-      </li>
-    ]
-  }
+  const authenticatedUser = () => [
+    <li key={1}>
+      <NavLink to="/clayshop">ClayShop</NavLink>
+    </li>,
+    <li key={2}>
+      <NavLink to="/logout">Logout</NavLink>
+    </li>
+  ]
 
   // Navbar for unauthenticated users
-  unauthenticatedUser() {
-    return (
-      <li>
-        <NavLink to="/login">User Access</NavLink>
-      </li>
-    )
-  }
+  const unauthenticatedUser = () => (
+    <li>
+      <NavLink to="/login">User Access</NavLink>
+    </li>
+  )
 
   // Navigation link only for Admin to create & delete user
-  adminNavLink() {
-    const { authenticated, userAccess } = this.props
-    return (
-      authenticated &&
-      userAccess === "Admin" && (
-        <li>
-          <a href="#">
-            Admin <span className="arrow">&#9660;</span>
-          </a>
-          <ul className="sub-menu">
-            <li>
-              <NavLink to="/signup">Add User</NavLink>
-            </li>
-            <li>
-              <NavLink to="/delete-user">Delete User</NavLink>
-            </li>
-          </ul>
-        </li>
-      )
-    )
-  }
-
-  render() {
-    const { authenticated } = this.props
-
-    return (
-      <div className="menu">
-        <ul>
+  const adminNavLink = () =>
+    authenticated &&
+    userAccess === "Admin" && (
+      <li>
+        <a href="#">
+          Admin <span className="arrow">&#9660;</span>
+        </a>
+        <ul className="sub-menu">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/signup">Add User</NavLink>
           </li>
-          {authenticated
-            ? this.authenticatedUser()
-            : this.unauthenticatedUser()}
-          {this.adminNavLink()}
+          <li>
+            <NavLink to="/delete-user">Delete User</NavLink>
+          </li>
         </ul>
-      </div>
+      </li>
     )
-  }
+
+  return (
+    <div className="menu">
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        {authenticated ? authenticatedUser() : unauthenticatedUser()}
+        {adminNavLink()}
+      </ul>
+    </div>
+  )
 }
 
 const mapStateToProps = ({
@@ -74,4 +60,4 @@ const mapStateToProps = ({
   userAccess
 })
 
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(NavBar)
--- src/pages/Redirects.js
+++ src/pages/Redirects.js
@@ -1,56 +1,44 @@
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
 
 // Redirect user after successful access
-export const AuthSuccess = () => {
-  return (
-    <div className="redirect">
-      <img src="public/images/access-granted.png" alt="access granted image" />
-      <h3>You have successfully entered</h3>
-    </div>
-  )
-}
+export const AuthSuccess = () => (
+  <div className="redirect">
+    <img src="public/images/access-granted.png" alt="access granted image" />
+    <h3>You have successfully entered</h3>
+  </div>
+)
 
 // Redirect user if no authorization for access
-export const AuthFailure = () => {
-  return (
-    <div className="redirect">
-      <img src="public/images/access-denied.png" alt="access denied image" />
-      <h3>
-        Access Denied.<br />
-        You are not authorized
-      </h3>
-    </div>
-  )
-}
+export const AuthFailure = () => (
+  <div className="redirect">
+    <img src="public/images/access-denied.png" alt="access denied image" />
+    <h3>
+      Access Denied.<br />
+      You are not authorized
+    </h3>
+  </div>
+)
 
 // Redirect after successful user creation
-class SignupSuccessPage extends Component {
-  render() {
-    return (
-      <div className="redirect">
-        <img src="public/images/new-user.png" alt="new user image" />
-        <h3>{this.props.signupSuccess}</h3>
-      </div>
-    )
-  }
-}
+export const SignupSuccessPage = ({ signupSuccess }) => (
+  <div className="redirect">
+    <img src="public/images/new-user.png" alt="new user image" />
+    <h3>{signupSuccess}</h3>
+  </div>
+)
 
 export const NewUserCreated = connect(({ auth: { signupSuccess } }) => ({
   signupSuccess
 }))(SignupSuccessPage)
 
 // Redirect after admin deleting an existing user
-class UserDeletedPage extends Component {
-  render() {
-    return (
-      <div className="redirect">
-        <img src="public/images/user-deleted.png" alt="delete user image" />
-        <h3>{this.props.deleteSuccess}</h3>
-      </div>
-    )
-  }
-}
+export const UserDeletedPage = ({ deleteSuccess }) => (
+  <div className="redirect">
+    <img src="public/images/user-deleted.png" alt="delete user image" />
+    <h3>{deleteSuccess}</h3>
+  </div>
+)
 
 export const UserDeleted = connect(({ auth: { deleteSuccess } }) => ({
   deleteSuccess
